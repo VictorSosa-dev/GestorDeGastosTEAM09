6e5902269c66b5581e050b682f5f3f1cdf2ca920
testusuario <pruebamorafoo@gmail.com>
2023-03-09 22:00:17 -0600

    -Creación de la clase User.kt -Creación de propiedades

---
+++ src/main/kotlin/mx/com/team9/models/User.kt
@@ -0,0 +1,26 @@
+package mx.com.team9.models
+
+const val PASSWORD_LENGHT = 10
+class User(name: String,password: String) {
+
+    init {
+        println("El usuario $name ha ingresado")
+    }
+
+    private var name = name
+    private var password: String = password
+        set(value) {
+            if (value.length >= PASSWORD_LENGHT) field = value
+            else println("El password debe contener al menos 10 digitos")
+        }
+
+
+
+    companion object {
+        @JvmStatic fun main(vararg input: String){
+            val u1 = User("juan", "os")
+        }
+
+    }
+
+}
