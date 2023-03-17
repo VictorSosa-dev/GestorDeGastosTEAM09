4799d699172eaeeaa633024834e9a6ce35544709
Emanuel River <emanuelmobiledeveloper01@gmail.com>
2023-03-16 22:41:44 -0600

    creacion de menu de opciones

--- src/main/kotlin/models/User.kt
+++ src/main/kotlin/models/User.kt
@@ -1,5 +1,7 @@
 package models
 
+import kotlin.reflect.typeOf
+
 const val PASSWORD_LENGHT = 10
 class User {
 
@@ -30,9 +32,38 @@ class User {
 
     companion object {
         @JvmStatic fun main(vararg input: String){
-            val u1 = User()
+            /*val u1 = User()
             u1.setUsuario("")
-            u1.setPassword("")
+            u1.setPassword("")*/
+
+            println("+".repeat(60))
+            println("+              BIENVENIDO A TU GESTOR DE GASTOS            +")
+            println("+                        AUTHOR TEAM 9                     +")
+            println("+  https://github.com/VictorSosa-dev/GestorDeGastosTEAM09/ +")
+            println("+".repeat(60))
+            println("*".repeat(60))
+            println("*                        MENU DE OPCIONES                  *")
+            println("*                       1.- INICIAR SESIÓN                 *")
+            println("*                       2.- CONSULTAR SALDO                *")
+            println("*                       3.- CONSULTAR MOVIMIENTOS          *")
+            println("*                       4.- SALIR                          *")
+            println("*".repeat(60))
+
+
+            var _salida = false
+            do {
+                print("PULSA TU SELECCION E INTRO:")
+                val opc = readln().toInt()
+                
+                when(opc){
+                    1 -> println("Haz elegido iniciar sesión")
+                    2 -> println("Haz elegido consultar saldo")
+                    4 -> _salida = true
+                    else -> println(" $opc ->>> no es una opción valida :(")
+                }
+
+                //_salida = true
+            }while (!_salida)
         }
 
     }
