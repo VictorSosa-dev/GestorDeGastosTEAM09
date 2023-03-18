065df85574da180f7335d194a77ea8cb2c7a054d
VictorSosa-dev <manuelsosapia@gmail.com>
2023-03-17 18:08:01 -0600

    Modificacion del menu

--- src/main/kotlin/mx/com/team9/models/User.kt
+++ src/main/kotlin/mx/com/team9/models/User.kt
@@ -1,7 +1,7 @@
 package mx.com.team9.models
 
 const val PASSWORD_LENGHT = 10
-class User(name: String,password: String) {
+class User(name: String, email: String, password: String) {
     /**
      * Clase que representa a un usuario
      * @param name Nombre del usuario
@@ -13,19 +13,34 @@ class User(name: String,password: String) {
     }
 
     private var name = name
+        set(value) {
+            if (value.length >= 3) field = value
+            else println("El nombre debe contener al menos 3 digitos")
+        }
+    private var email: String = email
+        set(value) {
+            if (value.contains("@")) field = value
+            else println("El email debe contener un @")
+        }
+
     private var password: String = password
         set(value) {
             if (value.length >= PASSWORD_LENGHT) field = value
             else println("El password debe contener al menos 10 digitos")
         }
 
-
-
-    companion object {
-        @JvmStatic fun main(vararg input: String){
-            val u1 = User("juan", "os")
-        }
-
+    fun getName(): String {
+        return this.name
     }
+
+    fun getEmail(): String {
+        return this.email
+    }
+
+    fun validatePassword(password: String): Boolean {
+        return this.password == password
+    }
+
+
 
 }
---
+++ src/main/kotlin/mx/com/team9/utils/Menu.kt
@@ -0,0 +1,81 @@
+package mx.com.team9.utils
+
+import mx.com.team9.models.Cuenta
+import mx.com.team9.models.User
+
+class Menu(){
+
+    private val listOfUser = mutableListOf<User>()
+
+    fun showMenu() {
+        println("+".repeat(60))
+        println("+              BIENVENIDO A TU GESTOR DE GASTOS            +")
+        println("+                        AUTHOR TEAM 9                     +")
+        println("+  https://github.com/VictorSosa-dev/GestorDeGastosTEAM09/ +")
+        println("+".repeat(60))
+        println("*".repeat(60))
+        println("*                        MENU DE OPCIONES                  *")
+        println("*                       1.- INICIAR SESIÓN                 *")
+        println("*                       2.- REGISTRARSE                    *")
+        println("*                       3.- SALIR                          *")
+        println("*".repeat(60))
+
+
+        var _salida = false
+        do {
+            print("PULSA TU SELECCION E INTRO:")
+            // evitar que el usuario ingrese un valor no numerico
+            val opc = readLine()?.toIntOrNull() ?: 0
+            when (opc) {
+                1 -> login()
+                2 -> singIn()
+                3 -> _salida = true
+                else -> println(" $opc ->>> no es una opción valida :(")
+            }
+
+            //_salida = true
+        } while (!_salida)
+    }
+
+    // login
+    private fun login(){
+        println("Ingresa tu email:")
+        val email = readLine().toString()
+        println("Ingresa tu contraseña:")
+        val password = readln().toString()
+        // Validar si el usuario ya existe
+        val existe = listOfUser.find { it.getEmail() == email }
+        if (existe == null){
+            println("El usuario $email no existe")
+            return
+        }
+    }
+
+    // sing in
+    private fun singIn(){
+        // Crear usuario
+        println("Ingresa tu usuario:")
+        val usuario= readLine().toString()
+        println("Ingresa tu email:")
+        val email = readLine().toString()
+        println("Ingresa tu contraseña:")
+        val password = readLine().toString()
+        val u1 = User(usuario, email, password)
+        // Validar si el usuario ya existe
+        val existe = listOfUser.find { it.getEmail() == email }
+        if (existe != null){
+            println("El usuario $usuario ya existe")
+            return
+        }
+        listOfUser.add(u1)
+
+    }
+
+
+    private fun mostrarMenuOperaciones(usuario: String){
+        println("+".repeat(60))
+        println("+                    Bienvenido $usuario                  +")
+        println("+                        AUTHOR TEAM 9                    +")
+        println("+".repeat(60))
+    }
+}
